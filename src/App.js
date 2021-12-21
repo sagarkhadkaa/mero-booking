20e192878aff71abf5c6bf4414fdc9d0b844e0e9
sagar <sagarkhadka2069@gmail.com>
2021-12-21 17:04:40 +0545

    Footer complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Button from "./components/button";
+import Footer from "./components/footer";
 import NavBar from "./components/navBar";
 function App() {
     return (
         <div className="App">
             <NavBar />
+
+            <Footer />
             {/* <Button /> */}
         </div>
     );
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <footer className="footer fixed-bottom py-3 bg-light ">
+            <div className="container">
+                <p className="text-muted text-center">
+                    Copyright 2021. Mero Booking Inc, Kathmandu
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
